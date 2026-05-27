9e272c285e416ea3a786a6741f68caa4b2163146
Bastiaan Boland <bastiaan.boland@gmail.com>
2026-05-27 17:43:26 +0900

    add pianoroll plotting code to helpers.py

--- sheets/helpers.py
+++ sheets/helpers.py
@@ -1,3 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_pianoroll_in_vs_out(ground_truth, prediction):
+    # TODO: Add switch for having time in frames vs seconds
+    fig, axes = plt.subplots(2, 1, figsize=(15, 8))
+
+    axes[0].set_title("Piano Roll (ground truth)")
+    axes[0].imshow(ground_truth, aspect="auto", origin="lower", cmap="Blues")
+    axes[0].set_xlabel("Time (frames)")
+    axes[0].set_ylabel("Piano key (0=A0, 87=C8)")
+
+    axes[1].set_title("Model output")
+    axes[1].imshow(prediction, aspect="auto", origin="lower", cmap="Blues")
+    axes[1].set_xlabel("Time (frames)")
+    axes[1].set_ylabel("Piano key (0=A0, 87=C8)")
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_pianoroll(piano_roll):
+    plt.figure(figsize=(15, 4))
+    plt.imshow(piano_roll, aspect="auto", origin="lower", cmap="Blues")
+
+    plt.title("Piano Roll (ground truth)")
+    plt.xlabel("Time (frames)")
+    plt.ylabel("Piano key (0=A0, 87=C8)")
+
+    plt.show()
 
 def _pad_or_trim(audio: np.ndarray, length: int) -> np.ndarray:
     if len(audio) >= length:
